66c7ed006c53448ad211d1f2997c8dcde5a07052
alawimartins <yasminmartins.gt@gmail.com>
2021-03-02 23:51:49 +0000

    added themeContext to project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
+import StyledTheme from "./global-style";
+import Button1 from "./components/Button1/Button1";
+import ToggleSwitch from "./components/ToggleSwitch/ToggleSwitch.jsx";
+import breakpoint from './utils/breakpoints';
+import xsHeader from './assets/mobile/bg-pattern-header.svg';
+import smHeader from './assets/tablet/bg-pattern-header.svg';
+import lgHeader from './assets/desktop/bg-pattern-header.svg';
+import logo from './assets/desktop/logo.svg'
 
-import MyImage from './assets/favicon-32x32.png';
-import './index.css';
+const theme = "light";
+export const ThemeContext = React.createContext(theme);
 
-console.log('HERE')
-const App = () => {
+const Wrapper = styled.div`
+  background-repeat: repeat-x;
+  @media ${breakpoint.device.xs}{
+    background-image: url(${xsHeader});
+  }
+  @media ${breakpoint.device.sm}{
+    background-image: url(${smHeader});
+  }
+  @media ${breakpoint.device.lg}{
+    background-image: url(${lgHeader});
+  }
+`;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: "light",
+      checked: true,
+    };
+
+  }
+  toggleTheme() {
+    this.setState({
+      theme: this.state.theme === "dark" ? "light" : "dark",
+      checked: !this.state.checked,
+    })
+  }
+
+  render() {
     return (
-        <div>
-            <div>Welcome to my-webpack-react-starter</div>
-            <img src={MyImage} />
-        </div>
+      <ThemeContext.Provider value={this.state.theme}>
+        <StyledTheme />
+        <Wrapper>
+          <img src={logo} />
+          <ToggleSwitch checked={this.state.checked} onChange={this.toggleTheme.bind(this)} />
+          <Button1 className="primary">Primary</Button1>
+          <Button1 className="secondary">Secondary</Button1>
+          <Button1 className="warn">Warning</Button1>
+          <Button1 className="disabled">Disabled</Button1>
+          <Button1 className="primary">Primary</Button1>
+          <Button1 className="secondary">Secondary</Button1>
+          <Button1 className="warn">Warning</Button1>
+          <Button1 className="disabled">Disabled</Button1>
+          <Button1 className="primary">Primary</Button1>
+          <Button1 className="secondary">Secondary</Button1>
+          <Button1 className="warn">Warning</Button1>
+          <Button1 className="disabled">Disabled</Button1>
+        </Wrapper>
+        <p>hello</p>
+      </ThemeContext.Provider>
     );
-};
+  }
+}
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
