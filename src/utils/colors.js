d27fa4506035d5a0556b64047b57d36af09a3ecb
alawimartins <yasminmartins.gt@gmail.com>
2021-03-02 23:50:53 +0000

    color variables

---
+++ src/utils/colors.js
@@ -0,0 +1,9 @@
+export const violet = "#5964E0";
+export const lightViolet = "#939BF4";
+export const veryDarkViolet = "#19202D";
+export const midnight = "#121721";
+export const white = "#FFFFFF";
+export const lightGrey = "#F4F6F8";
+export const grey = "#9DAEC2";
+export const darkGrey = "#9DAEC2";
+
