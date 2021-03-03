c831458eca62384b112b280d6a0b2f3c4d0bbd6c
alawimartins <yasminmartins.gt@gmail.com>
2021-03-03 15:46:11 +0000

    added checkbox and button to search bar

--- src/components/SearchArea/SearchArea.jsx
+++ src/components/SearchArea/SearchArea.jsx
@@ -2,25 +2,40 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import * as Styled from './searchArea.styles'
 import SearchBar from "./../SearchBar/SearchBar.jsx";
+import Checkbox from "./../CheckBox/Checkbox.jsx";
+import Button from "./../Button/Button.jsx";
 import { ThemeContext } from "../../index";
 import searchIcon from '../../assets/desktop/icon-search.svg'
 import locationIcon from '../../assets/desktop/icon-location.svg'
 
-const SearchArea = ({ onChange }) => {
-  const onSearchAreaChange = (event) => {
-    onChange(event.target.checked)
+
+class SearchArea extends React.Component {
+  state = { checked: true }
+
+  handleCheckboxChange = (event) => {
+    this.setState({ checked: event.target.checked })
   }
 
-  return (
-    <ThemeContext.Consumer>
-      {theme =>
-        <Styled.SearchWrapper>
-          <SearchBar theme={theme} icon={searchIcon} placeholder={"Filter by title, companies, expertise…"} />
-          <SearchBar theme={theme} icon={locationIcon} placeholder={"Filter by location…"} />
-        </Styled.SearchWrapper>
-      }
-    </ThemeContext.Consumer>
-  )
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {theme =>
+          <Styled.SearchWrapper theme={theme}>
+            <SearchBar theme={theme} icon={searchIcon} placeholder={"Filter by title, companies, expertise…"} />
+            <SearchBar theme={theme} icon={locationIcon} placeholder={"Filter by location…"} />
+            <label>
+              <Checkbox
+                checked={this.state.checked}
+                onChange={this.handleCheckboxChange.bind(this)}
+              />
+              <span style={{ marginLeft: 8 }}>Full Time Only</span>
+            </label>
+            <Button primary label="Search" />
+          </Styled.SearchWrapper>
+        }
+      </ThemeContext.Consumer>
+    )
+  }
 }
 
 SearchArea.propTypes = {
--- src/components/SearchArea/searchArea.styles.js
+++ src/components/SearchArea/searchArea.styles.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
+import { white, veryDarkBlue } from "../../utils/colors";
 
 export const SearchWrapper = styled.div`
-  width: 1110px;
+  width: 90%;
   margin: 38px auto;
   display:flex;
   border-radius: 3px;
+  height: 80px;
+  background-color: ${props => (props.theme === "light" ? white : veryDarkBlue)};
 `;
