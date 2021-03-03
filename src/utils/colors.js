068254851a76c0fada19523c5245a85f960dfa3a
alawimartins <yasminmartins.gt@gmail.com>
2021-03-03 11:58:24 +0000

    amended const name

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,6 +1,6 @@
 export const violet = "#5964E0";
 export const lightViolet = "#939BF4";
-export const veryDarkViolet = "#19202D";
+export const veryDarkBlue = "#19202D";
 export const midnight = "#121721";
 export const white = "#FFFFFF";
 export const lightGrey = "#F4F6F8";
