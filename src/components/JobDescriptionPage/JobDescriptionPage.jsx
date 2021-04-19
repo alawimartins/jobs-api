84732534af06f6480d6602b1bfdb067a8e75bac2
alawimartins <yasminmartins.gt@gmail.com>
2021-04-19 17:24:26 +0100

    added JobDescription to JobDescriptionPage

--- src/components/JobDescriptionPage/JobDescriptionPage.jsx
+++ src/components/JobDescriptionPage/JobDescriptionPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import SearchAreaTemplate from '../SearchAreaTemplate/SearchAreaTemplate.jsx'
 import HowToApply from '../HowToApply/HowToApply.jsx'
 import JobDescription from '../JobDescription/JobDescription.jsx'
 import CompanyHeader from '../CompanyHeader/CompanyHeader.jsx'
 import * as Styled from './jobDescriptionPage.styles'
-import Button from "../Button/Button.jsx";
 import { ThemeContext } from "../../index";
 
 class JobDescriptionPage extends React.Component {
@@ -23,7 +21,7 @@ class JobDescriptionPage extends React.Component {
           <div>
             <div theme={theme}>
               <CompanyHeader />
-              {/* <JobDescription /> */}
+              <JobDescription />
               <HowToApply />
             </div>
           </div>
