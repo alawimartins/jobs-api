82fbcbeec6b5a43679c0490fa540cc1634130b43
alawimartins <yasminmartins.gt@gmail.com>
2021-05-03 17:04:29 +0100

    added modal component

---
+++ src/components/Modal/modal.styles.js
@@ -0,0 +1,22 @@
+import styled, { css } from 'styled-components'
+import { violet, lightViolet, darkGrey, white } from "../../utils/colors";
+
+export const Button = styled.button`
+  background: ${violet};
+  border-radius: 3px;
+  border: none;
+  color: white;
+  margin: 0.5em 1em;
+  padding: 15px 40px;
+  font-family: 'Kumbh Sans',sans-serif;
+  cursor: pointer;
+  :hover { background: ${lightViolet};
+  }
+
+  ${props => props.primary && css`
+    background: ${props => (props.theme === "light" ? '#e8eafa' : darkGrey)};
+    color: ${props => (props.theme === "light" ? violet : white)};
+    :hover { background-color: ${props => (props.theme === "light" ? '#D4D7FF' : '#ccc')};
+    }
+  `}
+`;
