7bb020b4f9013f2fddc9dff605ce335723c9bc3c
alawimartins <yasminmartins.gt@gmail.com>
2021-05-09 22:56:18 +0100

    make jobs load 12 by 12

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -7,40 +7,44 @@ import ReactDOM from "react-dom";
 import PropTypes from 'prop-types';
 import SearchAreaTemplate from '../SearchAreaTemplate/SearchAreaTemplate.jsx'
 import ContentPreview from '../ContentPreview/ContentPreview.jsx'
-import JobDescription from '../JobDescription/JobDescription.jsx'
 import * as Styled from './landingPage.styles'
-import Button from "../Button/Button.jsx";
-import ToggleSwitch from "../ToggleSwitch/ToggleSwitch.jsx";
-
-import logo from '../../assets/desktop/logo.svg'
-import styled from "styled-components";
 import { useTheme, useThemeUpdate } from '../../theme/ThemeContext.js'
 
-const LandingPage = ({ jobs }) => {
+const LandingPage = ({ jobs, funcProp }) => {
   const theme = useTheme()
   const toggleTheme = useThemeUpdate()
   const [checked, setChecked] = useState(false)
+  const [currentJobs, setCurrentJobs] = useState(jobs)
 
   useEffect(() => {
     setChecked(!checked)
   }, [toggleTheme])
 
+  useEffect(() => {
+    setCurrentJobs(jobs.slice(0, 12))
+  }, [jobs])
+
+  const loadMore = () => {
+    if (currentJobs.length + 12 < jobs.length) {
+      setCurrentJobs(jobs.slice(0, currentJobs.length + 12))
+    } else {
+      funcProp()
+    }
+  }
 
   return (
     <div>
       <div theme={theme}>
         <SearchAreaTemplate />
         <Styled.Wrapper>
-          {jobs.map(job => {
-            if (job.company_logo !== null) {
-              return <ContentPreview key={job.id} job={job} />
-            }
+          {currentJobs.map(job => {
+            return <ContentPreview key={job.id} job={job} />
           })}
         </Styled.Wrapper>
       </div>
+      <button onClick={loadMore}>Add</button>
     </div>
   )
 }
-
 
 export default LandingPage
