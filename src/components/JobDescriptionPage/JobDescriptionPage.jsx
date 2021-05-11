21b17ed0782ce9eb29ca52a5c8990ed2ec0a8056
alawimartins <yasminmartins.gt@gmail.com>
2021-05-11 17:45:46 +0100

    getting correct job using id from slug

--- src/components/JobDescriptionPage/JobDescriptionPage.jsx
+++ src/components/JobDescriptionPage/JobDescriptionPage.jsx
@@ -5,23 +5,30 @@ import {
   useParams
 } from "react-router-dom";
 
-import PropTypes from 'prop-types';
 import HowToApply from '../HowToApply/HowToApply.jsx'
 import JobDescription from '../JobDescription/JobDescription.jsx'
 import CompanyHeader from '../CompanyHeader/CompanyHeader.jsx'
-import * as Styled from './jobDescriptionPage.styles'
 import { useTheme } from '../../theme/ThemeContext.js'
 
 const JobDescriptionPage = ({ jobs }) => {
   const theme = useTheme()
   let { slug } = useParams();
 
-  console.log('slug', slug)
+  const job = jobs.filter(x => {
+    if (x.id === slug) {
+      return x
+    } else {
+      return
+    }
+  })
+
+  const jobObj = job[0]
+
   return (
     <div>
       <div theme={theme}>
-        <CompanyHeader />
-        <JobDescription />
+        <CompanyHeader job={jobObj} />
+        <JobDescription job={jobObj} />
         <HowToApply />
       </div>
     </div>
